fc3c6c79b472d2b2d1466d0f40ab4469965aedb5
anabneri <$git_config_user_email>
2020-05-18 18:17:25 -0300

    :zap: about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,86 @@
-import React from 'react'
+import React from "react"
 
-import Layout from "../components/Layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout/"
+import SEO from "../components/Seo"
+import SocialLinks from "../components/SocialLinks"
 
+import { MainContent } from "../styles/base"
 
 const AboutPage = () => (
-    <Layout>
-        <SEO title="About" />
-    <h1>About me</h1>  
-    </Layout>
+  <Layout>
+    <SEO
+      title="About me"
+      description="Learn more about this curious developer"
+    />
+    <MainContent>
+      <h1>About me </h1>
+      <p>
+        Hi I'm Ana Beatriz, Java fullstack developer, I am currently a Software
+        Engineer at{" "}
+        <a
+          href="https://www.itau.com.br/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Itaú Bank from Brazil
+        </a>
+      </p>
+
+      <p>
+        I'm really curious, always wanting to learn new things (this blog is
+        proof of that). I like and get along with Java, but I really enjoy
+        venturing into infra, linux and also the front end. In addition to being
+        a developer I'm a technology speaker and I go around coordinating the
+        <strong>Sou Java, Womakerscode and Unicorntech communities.</strong>
+      </p>
+
+      <p>
+        I love to learn and I love to teach, when I'm not working I'm writing
+        some article, or producing some content for the community! You see my
+        articles already published on{" "}
+        <a
+          href="https://dev.to/anabneri"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dev.to
+        </a>
+        , and follow the most recent ones right here :)
+      </p>
+
+      <h2>Skills</h2>
+
+      <ul>
+        <li>
+          {" "}
+          <span role="img" aria-label="Girl computer">
+            👩‍💻
+          </span>{" "}
+          Java Developer
+        </li>
+        <li>
+          {" "}
+          <span role="img" aria-label="Girl computer">
+            ✨
+          </span>{" "}
+          NoSQL and SQL databases
+        </li>
+        <li>
+          {" "}
+          <span role="img" aria-label="Girl computer">
+            ✨
+          </span>{" "}
+          NoSQL and SQL databases
+        </li>
+      </ul>
+
+      <h2>Contact me</h2>
+
+      <p>Follow me, let's be friends :D</p>
+
+      <SocialLinks hideStyle />
+    </MainContent>
+  </Layout>
 )
 
 export default AboutPage
